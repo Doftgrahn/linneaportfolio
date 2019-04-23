062b186c99f1ccdcc6d21caabae60c65f0155506
Simon grahn <simon.grahn@gmail.com>
2019-04-23 17:35:10 +0200

    Updates

---
+++ precache-manifest.a3c97a5da1d83055b9bd8e6da8fbf14f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d3215b8b5f56eb1c681",
+    "url": "/linneaportfolio/static/css/main.75143d8b.chunk.css"
+  },
+  {
+    "revision": "7d3215b8b5f56eb1c681",
+    "url": "/linneaportfolio/static/js/main.2f5518e1.chunk.js"
+  },
+  {
+    "revision": "940ab706580a63525a03",
+    "url": "/linneaportfolio/static/js/runtime~main.f09bcf83.js"
+  },
+  {
+    "revision": "38cb9f70210442f0d48b",
+    "url": "/linneaportfolio/static/js/2.baaaeeda.chunk.js"
+  },
+  {
+    "revision": "11938f63d1820e626fba0ab036aabf8d",
+    "url": "/linneaportfolio/static/media/instagram.11938f63.svg"
+  },
+  {
+    "revision": "268767188d7884813bc0258e8a86d2f9",
+    "url": "/linneaportfolio/static/media/facebook.26876718.svg"
+  },
+  {
+    "revision": "2bf10b9bd4c941ed26650ccc82de8e3e",
+    "url": "/linneaportfolio/static/media/LinneaTop.2bf10b9b.png"
+  },
+  {
+    "revision": "188d991961240f49d3da519a4a1a46a8",
+    "url": "/linneaportfolio/static/media/LinneaBottom.188d9919.jpg"
+  },
+  {
+    "revision": "53f300b66df67bd8c268f4d80c131798",
+    "url": "/linneaportfolio/index.html"
+  }
+];
