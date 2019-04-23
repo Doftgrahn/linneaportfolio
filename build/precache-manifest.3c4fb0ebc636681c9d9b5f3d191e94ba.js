d88b5916eb1f38d09b4e4cc97f314d76b83a8b1c
Simon grahn <simon.grahn@gmail.com>
2019-04-23 17:25:14 +0200

    build

---
+++ build/precache-manifest.3c4fb0ebc636681c9d9b5f3d191e94ba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "7793a85a1f8594f9ed1e",
+    "url": "/static/css/main.75143d8b.chunk.css"
+  },
+  {
+    "revision": "7793a85a1f8594f9ed1e",
+    "url": "/static/js/main.fe1f7f0b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "38cb9f70210442f0d48b",
+    "url": "/static/js/2.baaaeeda.chunk.js"
+  },
+  {
+    "revision": "11938f63d1820e626fba0ab036aabf8d",
+    "url": "/static/media/instagram.11938f63.svg"
+  },
+  {
+    "revision": "268767188d7884813bc0258e8a86d2f9",
+    "url": "/static/media/facebook.26876718.svg"
+  },
+  {
+    "revision": "2bf10b9bd4c941ed26650ccc82de8e3e",
+    "url": "/static/media/LinneaTop.2bf10b9b.png"
+  },
+  {
+    "revision": "188d991961240f49d3da519a4a1a46a8",
+    "url": "/static/media/LinneaBottom.188d9919.jpg"
+  },
+  {
+    "revision": "03ce0116e61bc4092b63ca78fe9332d7",
+    "url": "/index.html"
+  }
+];
